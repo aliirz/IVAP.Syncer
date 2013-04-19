2770ba1f10d292579d40e7db9415f42c0f96eea0
Ali Raza <alirz.uet@gmail.com>
2013-04-19 19:36:04 +0500

    initial commit

---
+++ IVAP.Syncer/Syncrhonizerer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IVAP.Syncer
+{
+    public class Syncrhonizerer
+    {
+        public List<SyncedSurvey> syncedSurvey(List<Survey> unsyncedSurvey)
+        {
+            List<SyncedSurvey> synced = new List<SyncedSurvey>();
+            foreach (Survey s in unsyncedSurvey)
+            {
+                //First update the DistrictF
+            }
+
+            return synced;
+        }
+    }
+}
